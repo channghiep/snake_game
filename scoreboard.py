8021517267a497a834a364d257a652181a9d5e3d
Chan Nghiep <ncnghiep1705@gmail.com>
2021-06-13 22:05:07 -0700

    Final version

--- scoreboard.py
+++ scoreboard.py
@@ -7,7 +7,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("high_score.txt") as file:
+            self.high_score = file.read()
         self.color("white")
         self.penup()
         # self.goto(0,270)
@@ -33,7 +34,9 @@ class Scoreboard(Turtle):
         self.update_score()
 
     def reset(self):
-        if self.score > self.high_score:
+        if self.score > int(self.high_score):
             self.high_score = self.score
+            with open("high_score.txt", mode='w') as file:
+                file.write(f"{self.score}")
         self.score = 0
         self.update_score()
